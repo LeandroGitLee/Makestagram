4123f1f5924128c59a5bbe3f4759d45021a5c9c8
Santi Gracia <santigracia@santigracia.com>
2015-06-29 23:59:38 -0700

    Added Images to all tabs. Added 3 ViewControllers for each tab Working on photo library or camera selection.

---
+++ Makestagram/PhotoTaking/PhotoTakingHelper.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTakingHelper.swift
+//  Makestagram
+//
+//  Created by Santi on 6/29/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTakingHelper: NSObject {
+   
+}
